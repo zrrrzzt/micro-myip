ee983a2906449a84adae9874518a7584970bb8c2
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-02-15 15:24:34 +0100

    Some changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { send } = require('micro')
 
 module.exports = async (request, response) => {
   const payload = {
-    'x-forwarded-for': request.headers['x-forwarded-for'] || false,
+    'xForwardedFor': request.headers['x-forwarded-for'] || false,
     remoteAddress: request.connection.remoteAddress || false
   }
   send(response, 200, payload)
